b5829fe950b74a8c177b46add8943bf2c9da80dd
pravinksavant <pravinksavant@gmail.com>
2021-09-03 20:34:53 +0530

    modified

--- aws-sg.tf
+++ aws-sg.tf
@@ -12,16 +12,6 @@ resource "aws_security_group_rule" "allow_http"{
     
 }
 
-resource "aws_security_group_rule" "allow_http"{
-    type = "ingress"
-    security_group_id = "${aws_security_group.webserver_sg.id}"
-    from_port = var.http_port
-    to_port = var.http_port
-    protocol = "tcp"
-    cidr_blocks = [var.from_source]
-    
-}
-
 resource "aws_security_group_rule" "allow_ssh"{
     type = "ingress"
     security_group_id = "${aws_security_group.webserver_sg.id}"
